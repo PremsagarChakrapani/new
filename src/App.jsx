47bdd5c1c0b3c6436edf789a6d7924a4037ebe55
PremsagarChakrapani <premsagarc11@gmail.com>
2024-08-11 10:11:13 +0530

    "footer update"

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import Footer from './components/footer';
 import Home from './pages/Home';
 import DisasterDetails from './pages/DisasterDetails';
 import Resources from './pages/Resources';
